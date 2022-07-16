6b623e2b119b6317924bf7122027ade03fe377b1
tdelgado00 <tdelgado00@github.com>
2022-07-16 17:08:50 -0300

    first version

---
+++ agent.py
@@ -0,0 +1,109 @@
+import numpy as np
+from game import TenThousand
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def bestAction(self, obs):
+        raise NotImplementedError
+
+    def play(self, env, eps):
+        results = []
+        for ep in range(eps):
+            obs = env.reset()
+            while obs is not None:
+                obs = env.step(self.bestAction(obs))
+            results.append(env.score)
+        return np.mean(results)
+
+
+class RandomAgent(Agent):
+    def __init__(self):
+        super().__init__()
+
+    def bestAction(self, obs):
+        if type(obs) == list:
+            return np.random.randint(len(obs))
+        else:
+            return np.random.rand() < 0.1  # probability of continuing
+
+
+class QAgent(Agent):
+    def __init__(self, verbose=False):
+        super().__init__()
+
+        self.verbose = verbose
+        self.v = np.zeros((5, 10000 // 50))
+        # v[i, j] is the expected value after choosing an option that leaves us
+        # with j points and i dices, before throwing the dice, but assuming that we will continue
+
+        self.n = np.zeros((5, 10000 // 50))
+
+    def train2(self, env, steps):
+        while steps >= 0:
+            print("Epoch done", steps)
+            for to_throw in range(1, 6):
+                for points in range(0, 3000):
+                    obs = env.set(to_throw, points)
+                    # dice have been thrown, we have to choose an option
+                    i = env.to_throw - 1
+                    j = env.points // 50
+
+                    target = 0 if obs is None else max(max(self.eval(obs)), max([s[1] for s in obs]))
+
+                    self.n[i, j] += 1
+                    self.v[i, j] += (target - self.v[i, j]) / self.n[i, j]
+
+                    steps -= 1
+
+    def train(self, env, steps):
+        obs = env.reset()
+        for t in range(steps):
+            # dice have been thrown, we have to choose an option
+            i = env.to_throw - 1
+            j = env.points // 50
+
+            target = 0 if obs is None else max(max(self.eval(obs)), max([s[1] for s in obs]))
+
+            self.n[i, j] += 1
+            self.v[i, j] += (target - self.v[i, j]) / self.n[i, j]
+
+            if obs is None:
+                obs = env.reset()
+                continue
+
+            a = np.random.randint(len(obs))
+
+            env.step(a)
+            obs = env.step(True)  # when training, the agent always continues
+
+    def eval(self, obs):
+        return [self.v[s[0]-1, s[1] // 50] for s in obs]
+
+    def bestAction(self, obs):
+        if type(obs) == list:
+            if max(self.eval(obs)) <= max([s[1] for s in obs]):
+                return np.argmax([s[1] for s in obs])  # if nothing is good for continuing, I break
+            else:
+                return np.argmax(self.eval(obs))
+        else:
+            return self.v[obs[0]-1, obs[1] // 50] > obs[1]
+
+
+if __name__ == "__main__":
+    env = TenThousand(verbose=False)
+    agent = RandomAgent()
+    print("Random agent performance", agent.play(env, 50000))
+
+    agent = QAgent(verbose=False)
+    print("Starting agent performance", agent.play(env, 50000))
+
+    print("Training...")
+    agent.train2(env, 500000)
+
+    print("Trained agent performance", agent.play(env, 50000))
+
+    np.savetxt("v.csv", agent.v, fmt="%.2f")
+    np.savetxt("n.csv", agent.n, fmt="%.2f")
---
+++ game.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+EPSILON = 0.1
+
+
+# Environmnent for one turn of the TenThousand game (we don't consider the distance to the 10000 mark)
+class TenThousand:
+
+    def __init__(self, verbose=False):
+        self.points = None
+        self.to_throw = None
+        self.afterstates = None
+        self.verbose = verbose
+        self.dice_thrown = None
+        self.score = None
+
+    def set(self, to_throw, points):
+        self.to_throw = to_throw
+        self.points = points
+        self.score = None
+        self.dice_thrown = False
+        self.throw_dice()
+        if len(self.afterstates) == 0:
+            return None
+        return self.afterstates
+
+    def throw_dice(self):
+        dice = np.random.randint(6, size=self.to_throw) + 1
+
+        if self.verbose:
+            print("Dice throw!", dice)
+
+        quantities = np.zeros(6, dtype=np.int32)
+        for d in dice:
+            quantities[d-1] += 1
+
+        dice_possibilities = [[] for _ in range(6)]
+
+        for i in range(quantities[0] + 1):
+            if i < 3:
+                dice_possibilities[0].append((i, 100 * i))
+            else:
+                dice_possibilities[0].append((i, 100 * (i - 3) + 1000))
+
+        for i in range(quantities[4] + 1):
+            if i < 3:
+                dice_possibilities[4].append((i, 50 * i))
+            else:
+                dice_possibilities[4].append((i, 50 * (i - 3) + 500))
+
+        for dice in [1, 2, 3, 5]:
+            if quantities[dice] >= 3:
+                dice_possibilities[dice].append((3, (dice + 1) * 100))
+            dice_possibilities[dice].append((0, 0))
+
+        throw_points = [0 for _ in range(self.to_throw + 1)]
+        q = [[]]
+        while len(q) > 0:
+            c = q.pop()
+            if len(c) == 6:
+                dice_used = sum([d[0] for d in c])
+                points = sum([d[1] for d in c])
+                throw_points[dice_used] = max(points, throw_points[dice_used])
+            else:
+                for poss in dice_possibilities[len(c)]:
+                    q.append(c + [poss])
+
+        self.afterstates = []
+        for throw in range(1, self.to_throw + 1):
+            next_throw = self.to_throw - throw
+            if next_throw == 0:
+                next_throw = 5
+            if throw_points[throw] > 0:
+                self.afterstates.append((next_throw, self.points + throw_points[throw]))
+
+        if self.verbose:
+            print(self.afterstates)
+
+        self.dice_thrown = True
+
+    def step(self, a):
+        if self.dice_thrown:
+            assert a < len(self.afterstates)
+
+            if self.verbose:
+                print("Selected", self.afterstates[a])
+
+            self.to_throw, self.points = self.afterstates[a]
+            self.dice_thrown = False
+            return self.to_throw, self.points
+        else:
+            if not a:
+                if self.verbose:
+                    print("Breaking with", self.points)
+                self.score = self.points
+                return None
+            else:
+                if self.verbose:
+                    print("Continuing")
+                self.throw_dice()
+                if len(self.afterstates) == 0:
+                    if self.verbose:
+                        print("Lost everything")
+                    self.score = 0
+                    return None
+                return self.afterstates
+
+    def reset(self):
+        if self.verbose:
+            print("Starting turn *****************")
+        self.points = 0
+        self.to_throw = 5
+        self.throw_dice()
+        if len(self.afterstates) == 0:
+            self.score = 0
+            return None
+        else:
+            self.score = None
+            return self.afterstates
+
+
+if __name__ == "__main__":
+    env = TenThousand(verbose=True)
+    obs = env.reset()
+    while obs is not None:
+        obs, points = env.step(np.random.randint(len(obs)))
